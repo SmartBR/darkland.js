bef23d6c543b2bbe15a653ae00433e824b4802ed
= <gustavotropper@gmail.com>
2021-01-16 17:02:01 -0300

    event system

---
+++ index.js
@@ -0,0 +1,23 @@
+const EventEmitter = require("events")
+const darkLand = new EventEmitter()
+
+module.exports = {
+    darkLand,
+    validate: (client) => {
+        client.on("message", (message) => {
+            if (message.parent.name !== "API" || message.name !== "votes") return;
+
+            try {
+                const messageFormat = message.content.split(" -> ")
+                const userId = messageFormat[0]
+                const botId = messageFormat[1]
+
+                if (client.user.id !== botId || !client.users.cache.get(userId)) return;
+
+                darkLand.emit("vote", client.users.cache.get(userId))
+            }catch(err) {
+                console.error(err)
+            }
+        })
+    }
+}
