f806483b107e17988ffed3dfafc7ca54044859aa
= <gustavotropper@gmail.com>
2021-01-16 18:06:15 -0300

    remove module exports

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const EventEmitter = require("events")
 const darkLand = new EventEmitter()
 
-module.exports = {
-    darkLand,
-    validate: (client) => {
-        client.on("message", (message) => {
-            if (message.parent.name !== "API" || message.name !== "votes") return;
+function validate(client) {
+    client.on("message", (message) => {
+        if (message.parent.name !== "API" || message.name !== "votes") return;
 
-            try {
-                const messageFormat = message.content.split(" -> ")
-                const userId = messageFormat[0]
-                const botId = messageFormat[1]
+        try {
+            const messageFormat = message.content.split(" -> ")
+            const userId = messageFormat[0]
+            const botId = messageFormat[1]
 
-                if (client.user.id !== botId || !client.users.cache.get(userId)) return;
+            if (client.user.id !== botId || !client.users.cache.get(userId)) return;
 
-                darkLand.emit("vote", client.users.cache.get(userId))
-            }catch(err) {
-                console.error(err)
-            }
-        })
-    }
+            darkLand.emit("vote", client.users.cache.get(userId))
+        }catch(err) {
+            console.error(err)
+        }
+    })
 }
